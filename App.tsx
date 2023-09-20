2fb0e66054fb88e6c1d4b727e5865a9cab3553b3
ivy <dllmwl08@gmail.com>
2023-09-20 15:46:28 -0400

    nativewind fails to run

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hi!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
